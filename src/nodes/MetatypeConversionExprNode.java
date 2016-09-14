b78533880aa5aee559f4e8c49ddefc50d5609307
Marcel Rebouças <mscreboucas@gmail.com>
2016-09-14 09:12:59 -0300

    Adds the last part of the nodes.

---
+++ src/nodes/MetatypeConversionExprNode.java
@@ -0,0 +1,21 @@
+package nodes;
+
+import ast.ASTNode;
+import util.Util;
+
+public class MetatypeConversionExprNode extends ASTNode {
+	
+	public MetatypeConversionExprNode(String contents) {
+		super(contents);
+	}
+
+	@Override
+	public void prettyPrint(int ident) {
+		
+		String identation = Util.getTabsForIdentationAmmount(ident);
+		String className = this.getClass().getSimpleName();
+		System.out.println(identation + className + ": " + nodeInfo);
+		for (ASTNode child : children) { child.prettyPrint(ident + 1); }
+	}
+
+}
